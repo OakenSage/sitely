5253f16dfa68e803296ef2d1b6c37959d81aa791
The9Sage <slimmichael11@gmail.com>
2026-05-25 12:27:16 -0400

    docs ready

--- src/main.py
+++ src/main.py
@@ -1,20 +1,26 @@
+import sys
 import os
 import shutil
 from block_code import markdown_to_html_node, extract_title
 
-def generate_recurse(from_path, template_path, dest_path):
+if len(sys.argv)>=2:
+  basepath=sys.argv[1]
+else:
+  basepath="/"
+
+def generate_recurse(from_path, template_path, dest_path, basepath):
   files=os.listdir(from_path)
   for file in files:
       file_path=os.path.join(from_path,file)
       des_path=os.path.join(dest_path,file)
       if os.path.isfile(file_path):
         new_path= os.path.splitext(des_path)[0]+".html"
-        generate_page(file_path, template_path, new_path)
+        generate_page(file_path, template_path, new_path, basepath)
       else:
         os.mkdir(des_path)
-        generate_recurse(file_path,template_path,des_path)
+        generate_recurse(file_path,template_path,des_path,basepath)
 
-def generate_page(from_path, template_path, dest_path):
+def generate_page(from_path, template_path, dest_path, basepath):
   print(f"Generating page from {from_path} to {dest_path} using {template_path}")
   from_=open(from_path)
   template=open(template_path)
@@ -25,6 +31,8 @@ def generate_page(from_path, template_path, dest_path):
   title=extract_title(md)
   temp=temp.replace("{{ Title }}",title)
   temp=temp.replace("{{ Content }}",file)
+  temp=temp.replace('href="/',f'href="{basepath}')
+  temp=temp.replace('src="/',f'src="{basepath}')
   direct=os.path.dirname(dest_path)
   os.makedirs(direct,exist_ok=True)
   with open(dest_path,"w") as f:
@@ -43,11 +51,11 @@ def copy_path(path,to_path):
         copy_path(file_path,des_path)
 
 def main():
-  if os.path.exists("public"):
-    shutil.rmtree("public")
-  os.mkdir("public")
-  copy_path("static","public")
-  generate_recurse("content","template.html","public")
+  if os.path.exists("docs"):
+    shutil.rmtree("docs")
+  os.mkdir("docs")
+  copy_path("static","docs")
+  generate_recurse("content","template.html","docs",basepath)
 
 main()
 
